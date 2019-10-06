4481d33f782522c104ceefde7bdda37d92cf1f17
Paveluts42 <55815780+Paveluts42@users.noreply.github.com>
2019-10-06 15:35:37 +0300

    a

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Header = () => {
+    return (<header className="header">
+        <img src="http://klingonska.org/akademien/logo/ka-logo/ka-logo-color_transparent_bg.svg" />
+    </header >)
+};
+
+export default Header;
---
+++ src/components/Profil.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const Profil = () => {
+    return (<div className="content">
+        <div>
+            <img src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" />
+        </div>
+        <div>
+            ava+discription
+      </div>
+        <div>
+            My posts
+        </div>
+        <div>
+            New post
+        </div>
+
+        <div>
+
+            post 1
+          </div>
+        <div>
+            post 2
+          </div>
+    </div>)
+}
+export default Profil;
+
+
