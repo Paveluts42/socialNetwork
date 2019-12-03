026a0f61bc065cacac235ab925c14c123e28f31b
Paveluts42 <55815780+Paveluts42@users.noreply.github.com>
2019-12-03 10:40:47 +0300

    Updates

---
+++ precache-manifest.1532e01867e44097d35f6866b349ca52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d6fd8d4154d3e8d4fc56904ed96cf7",
+    "url": "/socialNetworkKama/index.html"
+  },
+  {
+    "revision": "badbdb3d2d8122111d0f",
+    "url": "/socialNetworkKama/static/css/main.8ce0a9d4.chunk.css"
+  },
+  {
+    "revision": "1efe083fca2ed6bc8c0e",
+    "url": "/socialNetworkKama/static/js/2.27041812.chunk.js"
+  },
+  {
+    "revision": "17909130ad46a998d849c07e93b72411",
+    "url": "/socialNetworkKama/static/js/2.27041812.chunk.js.LICENSE"
+  },
+  {
+    "revision": "badbdb3d2d8122111d0f",
+    "url": "/socialNetworkKama/static/js/main.1f57574d.chunk.js"
+  },
+  {
+    "revision": "b42cda2f48c0389f2fb2",
+    "url": "/socialNetworkKama/static/js/runtime-main.c4f791a4.js"
+  },
+  {
+    "revision": "92a4a31e145b5018e9be1ca4ae498e29",
+    "url": "/socialNetworkKama/static/media/ajax-loader.92a4a31e.gif"
+  }
+]);
