9b28ac757649a01bed88188788ccbd319dc52cb5
Paveluts42 <55815780+Paveluts42@users.noreply.github.com>
2019-12-03 10:25:58 +0300

    hh

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { Route } from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavbarContainer from "./components/NavbarContainer/NavbarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import ProfilContainer from "./components/Profil/ProfilContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/login/Login";
 import { connect } from "react-redux"
@@ -13,9 +15,6 @@ import { withRouter } from "react-router-dom"
 import { compose } from "redux"
 import { initializeApp } from "./redax/app-reducer "
 import Preloader from "./components/common/Preloader";
-import { withSuspense } from "./hoc/withSuspense";
-const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"))
-const ProfilContainer = React.lazy(() => import("./components/Profil/ProfilContainer"))
 
 class App extends React.Component {
   componentDidMount() {
@@ -31,13 +30,8 @@ class App extends React.Component {
         <HeaderContainer />
         <NavbarContainer />
         <div className="appWraperContent">
-          <Route path="/dialogs" render={(withSuspense(DialogsContainer))
-          } />
-          <Route path="/profile/:userId?" render={() => {
-            return (<React.Suspense fallback={<Preloader />}>
-              < ProfilContainer />
-            </React.Suspense>)
-          }} />
+          <Route path="/dialogs" render={() => <DialogsContainer />} />
+          <Route path="/profile/:userId?" render={() => <ProfilContainer />} />
           <Route path="/news" render={() => <News />} />
           <Route path="/music" render={() => <Music />} />
           <Route path="/settings" render={() => <Settings />} />
